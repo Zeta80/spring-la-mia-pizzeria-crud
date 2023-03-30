b38ee68aff566cf0513b87f65dfb8b9693492067
Mirko Robbi <mirko.robbi@gmail.com>
2023-03-30 15:49:56 +0200

    adjust

--- src/main/java/org/lessons/pizzery/springlamiapizzeriacrud/controllers/PizzaController.java
+++ src/main/java/org/lessons/pizzery/springlamiapizzeriacrud/controllers/PizzaController.java
@@ -1,13 +1,19 @@
 package org.lessons.pizzery.springlamiapizzeriacrud.controllers;
 import java.util.List;
+import java.util.Optional;
 
 import org.lessons.pizzery.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.pizzery.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/")
@@ -17,12 +23,25 @@ public class PizzaController {
 
 
     @GetMapping
-        public String index(Model model) {
-            List<Pizza> pizzas = pizzaRepository.findAll();
-            model.addAttribute("pizzas", pizzas);
-            return "/pizzas/index";
+    public String index(Model model) {
+        List<Pizza> pizzas = pizzaRepository.findAll();
+        model.addAttribute("list", pizzas);
+        return "pizzas/index";
+    }
+
+
+
+    @GetMapping("/{pizzaId}")
+    public String show(@PathVariable("pizzaId") Integer id, Model model){
+        Optional<Pizza> result = pizzaRepository.findById(id);
+        if(result.isPresent()){
+            model.addAttribute("pizza", result.get());
+            return "pizzas/show";
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
+}
 
 
 
--- src/main/java/org/lessons/pizzery/springlamiapizzeriacrud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/pizzery/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -5,8 +5,6 @@ import org.lessons.pizzery.springlamiapizzeriacrud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
-
-
     }
 
 
